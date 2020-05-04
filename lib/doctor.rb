ebc8cebccb106bab19a642eea3e4f3110566e69c
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 20:11:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,8 @@ class Doctor
 def self.all
   @@all
 end
-    def new_appointment(patient, date)
-Appointment.new(patient, self, date)
+    def new_appointment(date, patient)
+Appointment.new(date, patient, self)
 end
 
 def appointments
