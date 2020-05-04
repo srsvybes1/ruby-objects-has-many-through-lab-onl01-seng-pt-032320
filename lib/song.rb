e17a6f25817270054bf2397f4841632238f311e8
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 18:19:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,18 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  #initializes with a name and genre
-  #genre method makes Song belong to a genre
-  def initialize(name, genre)
+  @@all = []
+  def initialize(name, artist, genre)
     @name = name
+    @artist = artist
     @genre = genre
     genre.add_song(self)
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def artist
+    @artist
   end
 end
