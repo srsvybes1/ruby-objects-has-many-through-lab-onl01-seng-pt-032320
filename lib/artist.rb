906e22afd328e6e2eead34822d6c25b182934e7b
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 18:17:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,31 @@
 class Artist
-  attr_accessor :name
-  #songs belong to artist
-  #has many songs
-  #has many genres through its songs
+
+  attr_accessor :name, :artist, :genre
 @@all = []
-  #initialized with a name and an empty @songs array
   def initialize(name)
     @name = name
-    @songs = []
     @@all << self
   end
-def self.all
-  @@all
-end
-  #add_song takes in an argument of a songs
-  #adds that song to the artists collection of songs.
-  #tell the song that it belongs to the artist.
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  def songs
-     Artist.all.select {|artist| artist == self}
-   end
-  # #songs returns the @song array
-#  def songs
-#    @songs
-#  end
 
-  # #genres iterates over that artist's songs and collects the genre of each songs
-  def genres
-    self.songs.collect {|song| song.genre}
+  def self.all
+    @@all
   end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
 end
