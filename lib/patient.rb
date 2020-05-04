3599ea896cac1362302cfafbe71ab02e77bfa3b0
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-04 18:48:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,14 @@
 class Patient
 
   attr_accessor :name, :appointment, :docotr
-@@all = [] 
+@@all = []
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
-def self.all 
-  @@all 
-end 
+def self.all
+  @@all
+end
   def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
